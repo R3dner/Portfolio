1517e6c8434f498bacc12c37b9afc84543dec038
R3dner <matik_125@wp.pl>
2022-03-02 16:50:08 +0100

    init

---
+++ index.php
@@ -0,0 +1,83 @@
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <meta name="description" content="Strona przeznaczona do przedstawienia dorobku oraz umiejętności, które nabyłem.">
+	<title>Portfolio: Mateusz Dawid Kosior</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+            <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+            </div>
+        </div>
+    </nav>
+
+    <section class="container">
+        <div class="row mt-4">
+            <div class="col">
+                <p>inż. Mateusz Kosior</p>
+                <p>Ukończyłem technikum teleinformatyczne, oraz od niedawna jestem absolwentem Politechniki
+                    Świętokrzyskiej po kierunku <b>informatyka</b> ze specjalizajcją teleinformatyka.
+                    Obecnie zamierzam kształcić się w kierunku cyberbezpieczeństwa.</p>
+                <p>Informatyką interesuję się od wielu lat. Kierunkuje się w tematykę sieci komputerowych
+                    oraz tworzenia aplikacji internetowych. Posiadam kilka certykikatów CISCO.
+                    W trakcie studiów wykonałem kilka projektów zarówno sieciowych jak i programistycznych,
+                    które pozwoliły mi zdobyć podstawową wiedzę oraz umiejętności w tych dziedzinach.</p>
+                <p>Hobbystycznie zajmuję się naprawą/optymalizacją urządzeń takich jak laptopy, komputery PC oraz
+                    smartfony.
+                </p>
+
+            </div>
+
+            <div class="col carousel slide" id="carouselExampleIndicators" data-bs-ride="carousel"  >
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                    <img src="img/1.jpg" class="d-block w-100 rounded mx-auto img-fluid" alt="img">
+                    </div>
+                    <div class="carousel-item">
+                    <img src="img/2.jpg" class="d-block w-100 rounded mx-auto img-fluid" alt="img">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+        
+    </section>
+
+
+</body>
+</html>
